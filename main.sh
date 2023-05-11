bce46edbcc5549e056c014876bc905166e600e34
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-05-11 18:57:32 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,10 +3,15 @@
 DEBIAN_FRONTEND=noninteractive
 
 # Add dependent repositories
-wget url-of-pika-sources.deb -O pika-sources.deb
+wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
+add-apt-repository https://ppa.pika-os.com
+add-apt-repository ppa:pikaos/pika
+add-apt-repository ppa:kubuntu-ppa/backports
 apt install pika-sources.deb --yes --option Acquire::Retries=5 --option Acquire::http::Timeout=100 --option Dpkg::Options::="--force-confnew"
 # Clone Upstream
-mkdir -p ./debian ./src-pkg-name
+mkdir -p ./src-pkg-name
+cp -rvf ./debian ./src-pkg-name/
+cd ./src-pkg-name/
 
 # Get build deps
 ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
