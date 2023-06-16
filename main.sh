c93cc364c06cd662cc2f80982e985e5140a642dc
ferrreo <harderthanfire@gmail.com>
2023-06-16 18:21:57 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,20 +2,12 @@
 
 DEBIAN_FRONTEND=noninteractive
 
-# Add dependent repositories
-wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-add-apt-repository https://ppa.pika-os.com
-add-apt-repository ppa:pikaos/pika
-add-apt-repository ppa:kubuntu-ppa/backports
-apt install pika-sources.deb --yes --option Acquire::Retries=5 --option Acquire::http::Timeout=100 --option Dpkg::Options::="--force-confnew"
 # Clone Upstream
 mkdir -p ./src-pkg-name
 cp -rvf ./debian ./src-pkg-name/
 cd ./src-pkg-name/
 
 # Get build deps
-ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 apt-get build-dep ./ -y
 
 # Build package
