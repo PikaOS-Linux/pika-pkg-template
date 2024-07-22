f039cfaaa1f22bdbfda69d696f6cca5ccf844fa5
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-07-22 23:23:58 +0300

    v3 o3 lto

--- mainv3.sh
+++ mainv3.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 
-DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND="noninteractive"
+export DEB_BUILD_MAINT_OPTIONS="optimize=+lto -march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_CFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_CPPFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_CXXFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_LDFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_BUILD_OPTIONS="nocheck notest terse"
+export DPKG_GENSYMBOLS_CHECK_LEVEL=0
 
 # Clone Upstream
 mkdir -p ./src-pkg-name
